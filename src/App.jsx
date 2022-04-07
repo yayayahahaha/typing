a3683d3ec69d0138e39c4146b9158560ec2c5143
flyc <yayayahahahaooii@gmail.com>
2022-04-07 20:19:15 +0800

    顏色的部分好了、正在添加輸入中的顏色。撰寫了一些註解

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+// TODO 倒數計時?
+// TODO 打幾個字統計時間?
+// TODO 重置按鈕
+// TODO 套用設定按鈕: 時間多長、或是要打幾個字
+// TODO 定義所謂的 '一個字'
+
 import React, { useState } from 'react'
 
 import chanceInit from 'chance'
@@ -17,13 +23,19 @@ function App() {
   const keyUpHandler = function (event) {
     console.log('keyUpHandler')
     const code = event.code
-    if (code !== 'Space') return
-
     const checkInput = inputText.trim()
     const currentQuestion = textList[currentIndex].text
+    const reg = new RegExp(`^${checkInput}`) // NEXT TODO 這裡的判斷要詳細些?
+
+    if (code !== 'Space') {
+      // TODO typing 的時候中間打錯也要提示, 打對也要綠色
+      // if reg.test(currentQuestion)
+      textList[currentIndex].className = 'typing good'
+      return
+    }
+
     const pass = checkInput === currentQuestion
     textList[currentIndex].className = pass ? 'pass' : 'wrong'
-
     const nextIndex = currentIndex + 1
     setCurrentIndex(nextIndex) // 這個好像會有什麼問題我忘了
     textList[nextIndex].className = 'typing'
